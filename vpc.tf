576565ee074c3bab5919566407ee12a7a3b4bef7
Somani <anshsoma@8c85903acd02.ant.amazon.com>
2019-05-11 09:56:48 +1200

    added vpc resource

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,13 @@
-# # VPC Config Section
-# resource "aws_vpc" "vpc" {
-#   cidr_block  = "${var.vpc_base_cidr}"
+# VPC Config Section
+# ------------------------------------------------------------------------------------------------
+resource "aws_vpc" "vpc" {
+  cidr_block  = "${var.vpc_base_cidr}"
 
-#   enable_dns_hostnames  = true
-#   enable_dns_support    = true
+  enable_dns_hostnames  = true
+  enable_dns_support    = true
 
-#   tags {
-#     Name  = "${var.tag_env}_VPC"
-#     Env   = "${var.tag_env}"
-#   }
-# }
+  tags {
+    Name  = "${var.tag_env}_VPC"
+    Env   = "${var.tag_env}"
+  }
+}
